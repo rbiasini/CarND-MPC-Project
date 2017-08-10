3c2d47cfbf58590cd3aa67d6c44d321e228c6bda
Riccardo <riccardo.biasini@gmail.com>
2017-08-09 20:56:31 -0700

    added psi error

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -72,7 +72,7 @@ class FG_eval {
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) {
       fg[0] += 10*CppAD::pow(vars[cte_start + t], 2);
-      //fg[0] += 10*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 10*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += 0.05*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
